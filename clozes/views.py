37af48cc82b0c27fa35e106044829642112b5974
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2015-04-30 18:04:24 +0300

    Use get_object_or_404 shortcut

--- clozes/views.py
+++ clozes/views.py
@@ -1,5 +1,4 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
 from .models import Course, Deck, Card, TextChunk, BlankTextChunk
@@ -11,31 +10,21 @@ def index(request):
 
 
 def decks(request, course_id):
-    try:
-        course = Course.objects.get(id=course_id)
-    except Course.DoesNotExist:
-        raise Http404("Course does not exist")
+    course = get_object_or_404(Course, pk=course_id)
     deck_list = course.deck_set.all()
     return render(request, 'clozes/decks.html', {'course': course, 'deck_list': deck_list})
 
 
 def learn(request, course_id, deck_id):
-    try:
-        course = Course.objects.get(id=course_id)
-    except Course.DoesNotExist:
-        raise Http404("Course does not exist")
-    try:
-        deck = course.deck_set.get(id=deck_id)
-    except Deck.DoesNotExist:
-        raise Http404("Deck does not exist")
-
+    course = get_object_or_404(Course, pk=course_id)
+    deck = get_object_or_404(Deck, pk=deck_id)
     card_list = deck.card_set.all()
     return render(request, 'clozes/learn.html', {'course': course, 'deck': deck, 'card_list': card_list})
 
 
 def update(request, course_id, deck_id, card_id, chunk_id):
-    course = Course.objects.get(id=course_id)
-    deck = Deck.objects.get(id=deck_id)
-    card = Card.objects.get(id=card_id)
-    chunk = TextChunk.objects.get(id=chunk_id)
+    course = get_object_or_404(Course, pk=course_id)
+    deck = get_object_or_404(Deck, pk=deck_id)
+    card = get_object_or_404(Card, pk=card_id)
+    chunk = get_object_or_404(TextChunk, chunk_id)
     return HttpResponse('')
