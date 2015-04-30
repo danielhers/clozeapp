16f3ef1083879b7c3b6956d4085a587f89984c8f
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2015-04-30 16:24:21 +0300

    Basic view implementation, including update (for AJAX call)

--- clozes/urls.py
+++ clozes/urls.py
@@ -3,10 +3,13 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # /
+    # Course index, ex: /
     url(r'^$', views.index, name='index'),
-    # ex: /5/
+    # Deck index, ex: /5/
     url(r'^(?P<course_id>[0-9]+)/$', views.decks, name='decks'),
-    # ex: /5/14
+    # Main learning screen, ex: /5/14
     url(r'^(?P<course_id>[0-9]+)/(?P<deck_id>[0-9]+)/$', views.learn, name='learn'),
+    # Update chunk (AJAX), ex: /5/14/502
+    url(r'^(?P<course_id>[0-9]+)/(?P<deck_id>[0-9]+)/(?P<card_id>[0-9]+)/(?P<chunk_id>[0-9]+)/update$',
+        views.update, name='update'),
 ]
--- clozes/views.py
+++ clozes/views.py
@@ -1,16 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Course, Deck, Card, TextChunk, BlankTextChunk
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the clozes index.")
+    course_list = Course.objects.all()
+    output = ', '.join([c.name for c in course_list])
+    return HttpResponse(output)
 
 
 def decks(request, course_id):
-    response = "You're looking at the decks of course %s."
-    return HttpResponse(response % course_id)
+    course = Course.objects.get(id=course_id)
+    deck_list = course.deck_set.all()
+    output = "Decks in course %s:\n" % course.name
+    output += ', '.join([d.name for d in deck_list])
+    return HttpResponse(output)
 
 
 def learn(request, course_id, deck_id):
-    response = "You're looking at a card in deck %s, under course %s."
-    return HttpResponse(response % (deck_id, course_id))
+    course = Course.objects.get(id=course_id)
+    deck = Deck.objects.get(id=deck_id)
+    card_list = deck.card_set.all()
+    output = "Cards in deck %s, in course %s:\n" % (deck.name, course.name)
+    output += ', '.join([c.name for c in card_list])
+    return HttpResponse(output)
+
+
+def update(request, course_id, deck_id, card_id, chunk_id):
+    course = Course.objects.get(id=course_id)
+    deck = Deck.objects.get(id=deck_id)
+    card = Card.objects.get(id=card_id)
+    chunk = TextChunk.objects.get(id=chunk_id)
+    return HttpResponse('')
