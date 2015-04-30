549a98b7e9fea25a0b7a4337d0e08153d2c962a2
Daniel Hershcovich <danielh@cs.huji.ac.il>
2015-04-30 13:56:40 +0300

    Add basic models (user, course, deck, card, textChunk)

--- clozes/models.py
+++ clozes/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=50)
+
+class Course(models.Model):
+    name = models.CharField(max_length=50)
+
+class Deck(models.Model):
+    course = models.ForeignKey(Course)
+    user = models.ForeignKey(User)
+    name = models.CharField(max_length=50)
+
+class Card(models.Model):
+    deck = models.ForeignKey(Deck)
+    name = models.CharField(max_length=50)
+    next_appearance = models.DateField()
+    e_factor = models.IntegerField()
+
+class TextChunk(models.Model):
+    card = models.ForeignKey(Card)
+    index = models.IntegerField()
+    next_appearance = models.DateField()
+    e_factor = models.IntegerField()
+
