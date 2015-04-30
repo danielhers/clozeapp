a844921c1e61de4e42e05958a8122edc19854e1e
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2015-04-30 22:59:25 +0300

    Add view to skip card

--- clozes/urls.py
+++ clozes/urls.py
@@ -7,9 +7,10 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     # Deck index, ex: /learn/5/
     url(r'^learn/(?P<deck_id>[0-9]+)/$', views.learn, name='learn'),
-    # Deck index, ex: /insert/5/
-    url(r'^insert/(?P<deck_id>[0-9]+)/$', views.insert, name='insert'),
     # Update chunk (AJAX), ex: /update/504
-    url(r'^update/(?P<chunk_id>[0-9]+)/$',
-        views.update, name='update'),
+    url(r'^update/(?P<chunk_id>[0-9]+)/$', views.update, name='update'),
+    # Insert new card to deck, ex: /insert/5/
+    url(r'^insert/(?P<deck_id>[0-9]+)/$', views.insert, name='insert'),
+    # Skip card to get the next one, ex: /insert/24/
+    url(r'^skip/(?P<card_id>[0-9]+)/$', views.skip, name='skip'),
 ]
--- clozes/views.py
+++ clozes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
-from .models import Course, Deck, BlankTextChunk
+from .models import Course, Deck, Card, BlankTextChunk
 
 
 def index(request):
@@ -16,6 +16,12 @@ def learn(request, deck_id):
     return render(request, 'clozes/learn.html', {'card': card})
 
 
+def update(request, chunk_id):
+    blank = get_object_or_404(BlankTextChunk, pk=chunk_id)
+    blank.update_user_feedback(request.post)
+    return HttpResponse()
+
+
 def insert(request, deck_id):
     deck = get_object_or_404(Deck, pk=deck_id)
     if request.method == 'POST':
@@ -24,7 +30,9 @@ def insert(request, deck_id):
     return render(request, 'clozes/insert.html', {'deck': deck})
 
 
-def update(request, chunk_id):
-    blank = get_object_or_404(BlankTextChunk, pk=chunk_id)
-    blank.update_user_feedback(request.post)
+def skip(request, card_id):
+    card = get_object_or_404(Card, pk=card_id)
+    if request.method == 'POST':
+        card.skip()
+        return HttpResponseRedirect(reverse('learn', args=(card.deck.id,)))
     return HttpResponse()
