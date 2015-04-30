2918e10946ca4493d3d6ff59589fe7b6af4402a2
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2015-04-30 15:50:24 +0300

    Fix admin for TextChunk

--- clozes/admin.py
+++ clozes/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 
-from .models import Course, Deck, Card, TextChunk
+from .models import Course, Deck, Card, TextChunk, BlankTextChunk
 
 admin.site.register(Course)
 admin.site.register(Deck)
 admin.site.register(Card)
-admin.site.register(TextChunk)
+
+
+class TextChunkAdmin(admin.ModelAdmin):
+    fields = ['text', 'card', 'index']
+
+admin.site.register(TextChunk, TextChunkAdmin)
+
+
+class BlankTextChunkAdmin(admin.ModelAdmin):
+    fields = ['text', 'card', 'index', 'next_appearance', 'e_factor']
+
+admin.site.register(BlankTextChunk, BlankTextChunkAdmin)
