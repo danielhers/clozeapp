e9db8fb8939a6ba27e0b55d7ef1828ed8a51d708
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2015-04-30 16:55:41 +0300

    Templates for basic views

--- clozes/views.py
+++ clozes/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -6,25 +7,30 @@ from .models import Course, Deck, Card, TextChunk, BlankTextChunk
 
 def index(request):
     course_list = Course.objects.all()
-    output = ', '.join([c.name for c in course_list])
-    return HttpResponse(output)
+    return render(request, 'clozes/index.html', {'course_list': course_list})
 
 
 def decks(request, course_id):
-    course = Course.objects.get(id=course_id)
+    try:
+        course = Course.objects.get(id=course_id)
+    except Course.DoesNotExist:
+        raise Http404("Course does not exist")
     deck_list = course.deck_set.all()
-    output = "Decks in course %s:\n" % course.name
-    output += ', '.join([d.name for d in deck_list])
-    return HttpResponse(output)
+    return render(request, 'clozes/decks.html', {'course': course, 'deck_list': deck_list})
 
 
 def learn(request, course_id, deck_id):
-    course = Course.objects.get(id=course_id)
-    deck = Deck.objects.get(id=deck_id)
+    try:
+        course = Course.objects.get(id=course_id)
+    except Course.DoesNotExist:
+        raise Http404("Course does not exist")
+    try:
+        deck = course.deck_set.get(id=deck_id)
+    except Deck.DoesNotExist:
+        raise Http404("Deck does not exist")
+
     card_list = deck.card_set.all()
-    output = "Cards in deck %s, in course %s:\n" % (deck.name, course.name)
-    output += ', '.join([c.name for c in card_list])
-    return HttpResponse(output)
+    return render(request, 'clozes/learn.html', {'course': course, 'deck': deck, 'card_list': card_list})
 
 
 def update(request, course_id, deck_id, card_id, chunk_id):
