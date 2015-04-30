2e0fa88f47415e1698cfd7a8bb8e1c81b1df9df4
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2015-04-30 21:35:53 +0300

    Simplify URLs for index and learn

--- clozes/urls.py
+++ clozes/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     # Course index, ex: /
     url(r'^$', views.index, name='index'),
     # Deck index, ex: /5/
-    url(r'^(?P<course_id>[0-9]+)/(?P<deck_id>[0-9]+)/$', views.learn, name='learn'),
+    url(r'^(?P<deck_id>[0-9]+)/$', views.learn, name='learn'),
     # Update chunk (AJAX), ex: /5/14/502
-    url(r'^(?P<course_id>[0-9]+)/(?P<deck_id>[0-9]+)/(?P<card_id>[0-9]+)/(?P<chunk_id>[0-9]+)/update$',
+    url(r'^update/(?P<chunk_id>[0-9]+)/$',
         views.update, name='update'),
 ]
