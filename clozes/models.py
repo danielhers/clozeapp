68f1b5b1feea64225ea8dc877b7c24698bd1ce53
Daniel Hershcovich <danielh@cs.huji.ac.il>
2015-05-01 00:00:56 +0300

    Updated sample data + added "add card" method to deck

--- clozes/models.py
+++ clozes/models.py
@@ -18,6 +18,24 @@ class Deck(models.Model):
         card_set = self.card_set
         min_next = card_set.aggregate(Min('next_appearance'))['next_appearance__min']
         return card_set.get(next_appearance=min_next)
+    
+    
+    def add_card(self, card_name, text):
+        new_card = Card(id=None, deck=self, name=card_name, next_appearance=date.today())
+        new_card.save()
+        chunks = text.split("_")
+        for i, chunk in enumerate(chunks):
+            is_blank = i % 2 == 1
+            if is_blank:
+                new_chunk = BlankTextChunk(id=i, next_appearance=date.today() + timedelta(days=i), e_factor=1300)
+            else:
+                new_chunk = TextChunk(id=i)
+            new_chunk.text = chunk
+            new_chunk.card = new_card
+            new_chunk.index = i
+            new_chunk.save()
+
+        new_card.update_next_appearance()
 
 
 class Card(models.Model):
@@ -26,8 +44,10 @@ class Card(models.Model):
     next_appearance = models.DateField()
 
     def update_next_appearance(self):
-        blanks = Card.objects.all()[0].textchunk_set.filter(blanktextchunk__next_appearance__isnull=False)
+        blanks = self.textchunk_set.filter(blanktextchunk__next_appearance__isnull=False)
         self.next_appearance = blanks.aggregate(Min('blanktextchunk__next_appearance')).values()[0]
+        if self.next_appearance is None:
+            raise ValueError("Card has no blanks")
         self.save()
 
     def skip(self):
@@ -66,32 +86,25 @@ def dummy_interval_algorithm(blank, feedback):
     return 2
 
 
-def save_sample_text(text, card):
-    chunks = text.split("_")
-    # TODO this is probably wrong
-    for i, chunk in enumerate(chunks):
-        is_blank = i % 2 == 1
-        if is_blank:
-            new_chunk = BlankTextChunk(id=i, next_appearance=date.today() + timedelta(days=i), e_factor=1300)
-        else:
-            new_chunk = TextChunk(id=i)
-        new_chunk.text = chunk
-        new_chunk.card = card
-        new_chunk.index = i
-        new_chunk.save()
-    card.update_next_appearance()
-
 
 def insert_sample_data():
     # Create user
     sample_user = User.objects.create_user(username='daniel', email='dani@huji.com', password='1234')
-    sample_course = Course(id=1, name="היסטוריה עולמית")
-    sample_course.save()
-    sample_deck = Deck(id=1, course=sample_course, user=sample_user, name='היסטוריה עכשווית')
-    sample_deck.save()
-    sample_card = Card(id=1, deck=sample_deck, name='מלחמות העולם', next_appearance=date.today())
-    sample_card.save()
-    sample_text1 = "מלחמת העולם _הראשונה_ הסתיימה בשנת 1917"
-    save_sample_text(sample_text1, sample_card)
-    sample_text2 = "מלחמת העולם השנייה פרצה בשנת _1939_ ותמה בשנת _1945_"
-    save_sample_text(sample_text2, sample_card)
+
+    courses = [u"היסטוריה עולמית", u"מקרוכלכלה", u"C++"]
+    for i, course in enumerate(courses):
+        sample_course = Course(id=i, name=course[i])
+        sample_course.save()
+
+    topics = [u"היסטוריה עכשווית", u"המהפכה הצרפתית", u"ימי הביניים", u"העת העתיקה"]
+    for i, topic in enumerate(topics):
+        sample_deck = Deck(id=i, course=Course.objects.get(pk=0), user=sample_user, name=topics[i])
+        sample_deck.save()
+
+    sample_text1 = u"מלחמת העולם _הראשונה_ הסתיימה בשנת 1917"
+    sample_deck.add_card(u"מלחמה", sample_text1)
+    sample_text2 = u"מלחמת העולם השנייה פרצה בשנת _1939_ ותמה בשנת _1945_"
+    sample_deck.add_card(u"מלחמה", sample_text2)
+
+if __name__ == "__main__":
+    pass   # populate DB with simple data
